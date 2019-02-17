81a8952202d593103de21ea63105c748847fa79c
VanesaIG <VanesaIG9610@gmail.com>
2019-02-17 19:26:37 +0200

    Updates

---
+++ precache-manifest.f3292063435c958a736a3d21402eb064.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "de02d0693ead787c9df6",
+    "url": "/react-shop/static/js/runtime~main.de02d069.js"
+  },
+  {
+    "revision": "766e741b227c04b78c24",
+    "url": "/react-shop/static/js/main.766e741b.chunk.js"
+  },
+  {
+    "revision": "d31b206dee4aacf60a0e",
+    "url": "/react-shop/static/js/2.d31b206d.chunk.js"
+  },
+  {
+    "revision": "392a0e2bc7d8b428880ed3c8bf9cf4b5",
+    "url": "/react-shop/index.html"
+  }
+];
